4f00af2b45fbf3f5ccb5e1a9d2cbbea8d4702453
Seungyeon Lee <adie0423@gmail.com>
2018-11-26 22:15:37 +0900

    [추가] api-key class 추가

---
+++ src/main/java/com/example/seungyeonlee/hardcarry/Network/Key.java
@@ -0,0 +1,10 @@
+package com.example.seungyeonlee.hardcarry.Network;
+
+public class Key {
+    public static String getApiKey() {
+        return API_KEY;
+    }
+
+    public static String API_KEY = "";
+
+}
