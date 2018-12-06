6f9f525e17fc2c8a2509670a396907c13274bfde
Seungyeon Lee <adie0423@gmail.com>
2018-12-06 21:34:19 +0900

    [추가] 리그 정보 표시하기 - 티어, 승률 정보 표시함

--- src/main/java/com/example/seungyeonlee/hardcarry/DisplayResultActivity.java
+++ src/main/java/com/example/seungyeonlee/hardcarry/DisplayResultActivity.java
@@ -1,5 +1,6 @@
 package com.example.seungyeonlee.hardcarry;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,9 +10,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.seungyeonlee.hardcarry.Models.MatchList;
+import com.example.seungyeonlee.hardcarry.Models.League;
 import com.example.seungyeonlee.hardcarry.Network.API;
-import com.example.seungyeonlee.hardcarry.Network.Constant;
 import com.example.seungyeonlee.hardcarry.Network.Key;
 import com.example.seungyeonlee.hardcarry.Network.RiotAPI;
 
@@ -29,33 +29,134 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DisplayResultActivity extends AppCompatActivity {
-    private ImageView profileImg;
-    private TextView summonerName;
-    private String summonerAccountId;
+    private ImageView profileImg, tierImageView;
+    private TextView summonerName, tierTextView, leaguePointTextView, winTextView, loseTextView, winRateTextView;
+//    private ListView listView;
+
+    private String summonerAccountId, encryptedSummonerId;
     private String name;
     private Integer profileIconId;
 
     private Bitmap bitmap;
 
     private List matchList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_result);
 
-
+        // ImageView
         profileImg = (ImageView) findViewById(R.id.imageView);
+        tierImageView = (ImageView) findViewById(R.id.imageView3);
+
+        // TextView
         summonerName = (TextView) findViewById(R.id.textView);
+        tierTextView = (TextView) findViewById(R.id.tierTextView);
+        leaguePointTextView = (TextView) findViewById(R.id.leaguePointTextView);
+        winTextView = (TextView) findViewById(R.id.winTextView);
+        loseTextView = (TextView) findViewById(R.id.loseTextView);
+        winRateTextView = (TextView) findViewById(R.id.winRateTextView);
+
+
+//        listView = (ListView) findViewById(R.id.listView);
+
 
         // intent로 받아오기
         Intent intent = getIntent();
+
         summonerAccountId = intent.getStringExtra("summonerAccountId");
+        encryptedSummonerId = intent.getStringExtra("encryptedSummonerId");
         name = intent.getStringExtra("name");
         profileIconId = intent.getIntExtra("profileIconId", 1);
 
+        System.out.println(summonerAccountId);
+        System.out.println(encryptedSummonerId);
+        System.out.println(name);
+        System.out.println(profileIconId);
+
         // 소환사 이름
         summonerName.setText(name);
+        setProfileImg();
 
+
+
+        // retrofit 객체 생성
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RiotAPI.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        API api = retrofit.create(API.class);
+
+        Call<List<League>> call = api.getLeagueInfo(
+                Key.getApiKey(), encryptedSummonerId
+        );
+
+        call.enqueue(new Callback<List<League>>() {
+
+            @Override
+            public void onResponse(Call<List<League>> call, Response<List<League>> response) {
+                if (!response.body().isEmpty()) {
+                    System.out.println("여기다!!");
+//                    System.out.println(response.body().get(0).getWins());
+
+                    tierTextView.setText(response.body().get(0).getTier()+"\t"+response.body().get(0).getRank());
+                    leaguePointTextView.setText(response.body().get(0).getLeaguePoints().toString().concat("LP"));
+                    winTextView.setText(response.body().get(0).getWins().toString().concat("승"));
+                    loseTextView.setText(response.body().get(0).getLosses().toString().concat("패"));
+//                    System.out.println( (double) x / (double) y * 100.0 + "%");
+                    double rate =
+                            (float) ((double) response.body().get(0).getWins()
+                                                        / (double) (response.body().get(0).getLosses() + response.body().get(0).getWins()) *100.0);
+                    winRateTextView.setText("승률 \t"+Math.round(rate)+"%");
+
+                    setTierImg(response.body().get(0).getTier(), response.body().get(0).getRank());
+
+                } else {
+                    tierTextView.setText("무한한 가능성이 있는 언랭이에요:)");
+                    Toast.makeText(getApplicationContext(), "랭킹이 없네요:(", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<League>> call, Throwable t) {
+                System.out.println(t.getMessage());
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+//        Call<List<MatchList>> call = api.getMatchList(
+//                Key.getApiKey(), summonerAccountId
+//        );
+//
+//        call.enqueue(new Callback<List<MatchList>>() {
+//            @Override
+//            public void onResponse(Call<List<MatchList>> call, Response<List<MatchList>> response) {
+//                System.out.println("나오긴하냐");
+//                System.out.println(response.body());
+//
+//
+//                matchList = response.body();
+//
+//                for (Object aa: matchList
+//                        ) {
+//                    System.out.println(aa);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<MatchList>> call, Throwable t) {
+//                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+    }
+
+    private void setProfileImg(){
         // 소환사 프로필 사진 설정
 
         Thread mThread = new Thread() {
@@ -93,39 +194,65 @@ public class DisplayResultActivity extends AppCompatActivity {
             e.printStackTrace();
             Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
         }
+    }
+    private void setTierImg(String tier, String rank) {
+        String imgName;
+        switch (tier) {
+            case "CHALLENGER":
+                imgName = tier.toLowerCase();
+                break;
+            case "MASTER":
+                imgName = tier.toLowerCase();
+                break;
+            case "PLATINUM":
+                switch (rank) {
+                    case "I": imgName = tier.toLowerCase().concat("_i"); break;
+                    case "II": imgName = tier.toLowerCase().concat("_ii"); break;
+                    case "III": imgName = tier.toLowerCase().concat("_iii"); break;
+                    case "IV": imgName = tier.toLowerCase().concat("_iv"); break;
+                    default: imgName = tier.toLowerCase().concat("_v"); break;
 
-//        // retrofit 객체 생성
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl(RiotAPI.BASE_URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//
-//        API api = retrofit.create(API.class);
-//
-//        Call<List<MatchList>> m_call = api.getMatchList(
-//                Key.getApiKey(), summonerAccountId
-//        );
-//
-//        m_call.enqueue(new Callback<List<MatchList>>() {
-//            @Override
-//            public void onResponse(Call<List<MatchList>> call, Response<List<MatchList>> response) {
-//                System.out.println("나오긴하냐");
-//                System.out.println(response.body());
-//
-//
-//                matchList = response.body();
-//
-//                for (Object aa: matchList
-//                        ) {
-//                    System.out.println(aa);
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<MatchList>> call, Throwable t) {
-//                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
-//            }
-//        });
-
+                }
+            case "DIAMOND":
+                switch (rank) {
+                    case "I": imgName = tier.toLowerCase().concat("_i"); break;
+                    case "II": imgName = tier.toLowerCase().concat("_ii"); break;
+                    case "III": imgName = tier.toLowerCase().concat("_iii"); break;
+                    case "IV": imgName = tier.toLowerCase().concat("_iv"); break;
+                    default: imgName = tier.toLowerCase().concat("_v"); break;
+                }
+            case "GOLD":
+                switch (rank) {
+                    case "I": imgName = tier.toLowerCase().concat("_i"); break;
+                    case "II": imgName = tier.toLowerCase().concat("_ii"); break;
+                    case "III": imgName = tier.toLowerCase().concat("_iii"); break;
+                    case "IV": imgName = tier.toLowerCase().concat("_iv"); break;
+                    default: imgName = tier.toLowerCase().concat("_v"); break;
+                }
+            case "SLIVER":
+                switch (rank) {
+                    case "I": imgName = tier.toLowerCase().concat("_i"); break;
+                    case "II": imgName = tier.toLowerCase().concat("_ii"); break;
+                    case "III": imgName = tier.toLowerCase().concat("_iii"); break;
+                    case "IV": imgName = tier.toLowerCase().concat("_iv"); break;
+                    default: imgName = tier.toLowerCase().concat("_v"); break;
+                }
+            case "BRONZE":
+                switch (rank) {
+                    case "I": imgName = tier.toLowerCase().concat("_i"); break;
+                    case "II": imgName = tier.toLowerCase().concat("_ii"); break;
+                    case "III": imgName = tier.toLowerCase().concat("_iii"); break;
+                    case "IV": imgName = tier.toLowerCase().concat("_iv"); break;
+                    default: imgName = tier.toLowerCase().concat("_v"); break;
+                }
+            default:
+                imgName = "provisional";
+                break;
+        }
+        imgName.concat(".png");
+        Context context = tierImageView.getContext();
+        int id = context.getResources().getIdentifier(imgName, "drawable", context.getPackageName());
+        tierImageView.setImageResource(id);
+//        return imgName;
     }
 }
