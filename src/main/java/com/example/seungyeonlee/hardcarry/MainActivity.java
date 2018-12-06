88b9c1f987b6969712839f9303671b39301bcc88
Seungyeon Lee <adie0423@gmail.com>
2018-12-06 22:38:51 +0900

    [추가] 키보드 엔터 버튼 기능 추가 - 엔터 입력시 search 버튼을 누른 것과 동일한 기능하도록 함

--- src/main/java/com/example/seungyeonlee/hardcarry/MainActivity.java
+++ src/main/java/com/example/seungyeonlee/hardcarry/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -42,8 +43,18 @@ public class MainActivity extends AppCompatActivity {
         inputTextView = (TextView) findViewById(R.id.editText);
         searchBtn = (Button) findViewById(R.id.button);
 
-        // 프로필 이미지 사진 로드
 
+        // 키보드 enter 버튼으로 searchBtn 누를 수 있도록 함
+        inputTextView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    searchBtn.callOnClick();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         // 조회 버튼 클릭
         searchBtn.setOnClickListener(new View.OnClickListener() {
